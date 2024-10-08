982b3fcb161d2f1114d0e30f8cd9d28b9b0cb7bf
Thang Nguyen <nthanhthang24@gmail.com>
2024-10-08 20:57:49 +0700

    Adding Voucher

--- src/main/java/com/example/demo/models/BaseEntity.java
+++ src/main/java/com/example/demo/models/BaseEntity.java
@@ -1,8 +1,5 @@
 package com.example.demo.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,23 +7,12 @@ import java.time.LocalDateTime;
 
 @Setter
 @Getter
-@MappedSuperclass
 public abstract class BaseEntity {
 
-    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "created_on", nullable = false, updatable = false)
-    private LocalDateTime createdOn;
+    private Long createdOn;
 
-    @Column(name = "created_by", nullable = false)
     private Long createdBy;
-
-    @PrePersist
-    protected void onCreate() {
-        createdOn = LocalDateTime.now();
-    }
-
-    // Getters and Setters
 
 }
--- src/main/java/com/example/demo/models/VoucherAudit.java
+++ src/main/java/com/example/demo/models/VoucherAudit.java
@@ -1,42 +1,27 @@
 package com.example.demo.models;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@Entity
-@Table(name = "voucher_audit")
 public class VoucherAudit extends BaseEntity {
 
     // Getters and Setters
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // Primary key
 
-    @Column(name = "voucher_code", nullable = false)
     private String voucherCode;
 
-    @Column(name = "start_date", nullable = false)
     private Long startDate;
 
-    @Column(name = "end_date", nullable = false)
     private Long endDate;
 
-    @Column(name = "line_item_id", nullable = false)
     private String lineItemId;
 
-    @Column(name = "recipe_id", nullable = false)
     private Long recipeId;
 
-    @Column(name = "created_by", nullable = false)
-    private Long createdBy;
-
-    @Column(name = "account_id", nullable = false)
     private Long accountId;
 
-    @Column(name = "product_configuration_id", nullable = false)
     private Long productConfigurationId;
 
 }
